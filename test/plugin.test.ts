5c572613a68d2963b7eaffaf73aa1724dba97ccc
Juyeong Maing <mjy9088@naver.com>
2026-08-26 11:05:36 +0000

    Provide host state for admin route tests

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -58,7 +58,7 @@ describe("external URLs plugin", () => {
       }
     }]);
     close.push(() => registered.dispose());
-    const server = configuredDimAdminController({} as LifecycleOptions, registered);
+    const server = configuredDimAdminController({ stateRoot } as LifecycleOptions, registered);
     server.listen(0, "127.0.0.1");
     await once(server, "listening");
     close.push(() => new Promise((resolve) => server.close(() => resolve())));
